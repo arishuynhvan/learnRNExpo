b64fcb64a4f10398bc9091d078e44a041a64bcb4
arishuynhvan <arishuynhvan@gmail.com>
2020-05-12 16:59:52 +0700

    Mix stylesheet obj

--- App.js
+++ App.js
@@ -1,15 +1,41 @@
-import React from 'react';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 
 export default function App() {
   return (
-    <View style={{padding: 50}}>
-      <View>
-        <TextInput placeholder="Write your goal here" style={{height:50, marginBottom:10, borderBottomColor:"black", borderBottomWidth:1}}/>
-        <Button title="ADD"/>
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
+        <TextInput placeholder="Write your goal here" style={styles.input} />
+        <Button title="ADD" />
       </View>
-      <View></View>
+      <View>
+        <View style={[styles.flexbox, { backgroundColor: "red" }]}>
+          <Text style={{ color: "white" }}>1</Text>
+        </View>
+      </View>
     </View>
   );
 }
 
+const styles = StyleSheet.create({
+  screen: {
+    padding: 50,
+  },
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  input: {
+    height: "100%",
+    width: "80%",
+    borderBottomColor: "black",
+    borderBottomWidth: 1,
+  },
+  flexbox: {
+    width: "100%",
+    height: 50,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
--- CodingStyle.md
+++ CodingStyle.md
@@ -7,3 +7,19 @@ e.g. borderColor
 ## Quoted CSS name
 
 e.g. "border-color"
+
+## Inline style 
+
+Styles are set up directly inside the components -> bad :(
+
+=> Use StyleSheet object!
+
+## StyleSheet object
+
+- Good for future optimization
+- JS compiler can validate stylesheet to avoid silent errors
+- Shareable among different components
+
+## Formatting style
+- Prettier (now)
+- Babel
