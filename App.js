1f803736d842b6a7b41bae23a96576e9810605f6
arishuynhvan <arishuynhvan@gmail.com>
2020-05-12 16:30:02 +0700

    Add first text input and button

--- App.js
+++ App.js
@@ -1,19 +1,15 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={{padding: 50}}>
+      <View>
+        <TextInput placeholder="Write your goal here" style={{height:50, marginBottom:10, borderBottomColor:"black", borderBottomWidth:1}}/>
+        <Button title="ADD"/>
+      </View>
+      <View></View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
